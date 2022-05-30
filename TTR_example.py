261549d5d6f406b9197500ca838875c239058a41
dysun <sun117dingyi@gmail.com>
2022-05-30 14:45:14 -0700

    TTR solves reach problem with static obstacles

--- TTR_example.py
+++ TTR_example.py
@@ -17,7 +17,9 @@ g = Grid(minBounds=np.array([-3.0, -1.0, -math.pi]), maxBounds=np.array([3.0, 4.
 my_car = DubinsCar(uMode="min")
 
 # Initialize target set as a cylinder
-targeSet = CylinderShape(g, [2], np.array([0.0, 1.0, 0.0]), 0.70)
+targetSet = CylinderShape(g, [2], np.array([0.0, 1.0, 0.0]), 0.70)
+obstSet = CylinderShape(g, [2], np.array([2.0, 0.0, 0.0]), 0.70)
+
 po = PlotOptions("3d_plot", plotDims=[0,1,2], slicesCut=[],
                 min_isosurface=0, max_isosurface=0)
 
@@ -26,24 +28,26 @@ t_step = 0.05
 small_number = 1e-5
 tau = np.arange(start=0, stop=lookback_length + small_number, step=t_step)
 
-compMethod = { "PrevSetsMode": "minVWithV0"}
+compMethod = { "PrevSetsMode": "minVWithV0", "TargetSetMode": "max"}
 accuracy = "low"
-correct_result = HJSolver(my_car, g, targeSet,
+
+correct_result = HJSolver(my_car, g, [targetSet, obstSet],
                           tau, compMethod, po, accuracy)
 
 # -------------------------------- ONE-SHOT TTR COMPUTATION ---------------------------------- #
 g = Grid(minBounds=np.array([-3.0, -1.0, -math.pi]), maxBounds=np.array([3.0, 4.0, math.pi]),
-         dims=3, pts_each_dim=np.array([50, 50, 50]), periodicDims=[2])
+         dims=3, pts_each_dim=np.array([80, 80, 80]), periodicDims=[2])
 # Car is trying to reach the target
 my_car = DubinsCar(uMode="min")
 
 # Initialize target set as a cylinder
 targetSet = CylinderShape(g, [2], np.array([0.0, 1.0, 0.0]), 0.70)
+obstSet = CylinderShape(g, [2], np.array([2.0, 0.0, 0.0]), 0.70)
 po = PlotOptions( "3d_plot", plotDims=[0,1,2], slicesCut=[],
                   min_isosurface=lookback_length, max_isosurface=lookback_length)
 
 # First compute TTR set
 epsilon = 0.001
-V_0 = TTRSolver(my_car, g, targetSet, epsilon, po)
+V_0 = TTRSolver(my_car, g, [targetSet, obstSet], epsilon, po)
 
 #np.save("tt2_array.npy", V_0)
